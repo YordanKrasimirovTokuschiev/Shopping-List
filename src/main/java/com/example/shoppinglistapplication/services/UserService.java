d8283fdf54282ba4da4ce4ef4aaeb987029abf25
Yordan11021986 <yordan.tokuschiev@abv.bg>
2022-06-13 21:42:18 +0300

    implement userLogin business logic

--- src/main/java/com/example/shoppinglistapplication/services/UserService.java
+++ src/main/java/com/example/shoppinglistapplication/services/UserService.java
@@ -1,4 +1,9 @@
 package com.example.shoppinglistapplication.services;
 
+import com.example.shoppinglistapplication.models.service.UserServiceModel;
+
 public interface UserService {
+    void register(UserServiceModel userServiceModel);
+
+    UserServiceModel findByUsernameAndPassword(String username, String password);
 }
--- src/main/java/com/example/shoppinglistapplication/services/impl/UserServiceImpl.java
+++ src/main/java/com/example/shoppinglistapplication/services/impl/UserServiceImpl.java
@@ -1,8 +1,34 @@
 package com.example.shoppinglistapplication.services.impl;
 
+import com.example.shoppinglistapplication.models.entities.User;
+import com.example.shoppinglistapplication.models.service.UserServiceModel;
+import com.example.shoppinglistapplication.repositories.UserRepository;
 import com.example.shoppinglistapplication.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+
+    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper) {
+        this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public void register(UserServiceModel userServiceModel) {
+        userRepository.save(modelMapper.map(userServiceModel, User.class));
+
+    }
+
+    @Override
+    public UserServiceModel findByUsernameAndPassword(String username, String password) {
+       return userRepository
+               .findByUsernameAndPassword(username, password)
+               .map(user -> modelMapper.map(user, UserServiceModel.class))
+               .orElse(null);
+    }
 }
